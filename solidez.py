ec757a498f056d44ccf591c302866d7c5b861743
Lucas Santana <lucasstn10@gmail.com>
2018-05-04 11:58:50 -0400

    solidez V1 done

--- solidez.py
+++ solidez.py
@@ -1,8 +1,8 @@
-import math #floor
+import math # floor
 
-#implementa a solução dinâmica para a solidez do vetor, usa a solução da firmeza para divisão do problema em subestruturas ótimas
+# implementa a solução dinâmica para a solidez do vetor, usa a solução da firmeza para divisão do problema em subestruturas ótimas
 def solidezV3(A, p, r) :
-	firmeza = [None]*r
+	firmeza = [0]*r
 	firmeza[p] = A[p]
 
 	for i in range(p+1, r):
@@ -19,7 +19,7 @@ def solidezV3(A, p, r) :
 
 	return x
 
-#função utilizada na versão 2 do solidez
+# função utilizada na versão 2 do solidez
 def solidez_meio(A, p, q, r):
 	B = [None]*r
 	B = A
@@ -42,19 +42,19 @@ def solidez_meio(A, p, q, r):
 	return aux + aux2
 
 
-#implementa a solução para o problema da solidez usando a técnica da divisão e conquista
+# implementa a solução para o problema da solidez usando a técnica da divisão e conquista
 def solidezV2(A, p, r):
-	B = [None]*r
-	B = A
-	valores = []
+  
+	valores = [0]*r
+	
 	if p == r:
-		return B[p]
+		return A[p]
 
 	else:
 		q = math.floor(((p+r) / 2))
-		x = solidezV2(B, p, q)
-		y = solidezV2(B, q+1, r)
-		z = solidez_meio(B, p, q, r)
+		x = solidezV2(A, p, q)
+		y = solidezV2(A, q+1, r)
+		z = solidez_meio(A, p, q, r)
 
 		valores.append(x)
 		valores.append(y)
@@ -62,6 +62,36 @@ def solidezV2(A, p, r):
 
 		return max(valores)
 
-def solidez(A, p, r):
-	pass
-	
+def solidezV1(A, p, r):
+  soma = [0]*r
+
+  # se o intervalo é unitário
+  if p == r:
+    return A[p]
+
+  i, j = p, 0
+  
+  while (j < r):
+    
+    while (i < r) and (A[i] >= 0):
+      soma[j] += A[i]
+      i += 1
+	  
+	# se somou até o número máximo de elementos
+    if i == r:
+      # sai do primeiro laço
+      j = r
+    # se ainda restam elementos para análise 
+    else:
+      j += 1
+      # incrementa o i para a continuação do cálculo
+      i += 1
+  
+  return max(soma)
+
+def main ():
+  a = [1,1,1,0,-1,1]
+  print("soma máxima:", solidezV2(a, 0, len(a)))
+  
+  
+main()

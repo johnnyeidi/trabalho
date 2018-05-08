00d86880c5c2d3f6b02c40ff7735940c927547d7
Lucas Santana <lucasstn10@gmail.com>
2018-05-08 16:15:16 -0400

    all done

--- solidez.py
+++ solidez.py
@@ -6,7 +6,7 @@ def solidezV3(A, p, r) :
 	firmeza[p] = A[p]
 
 	for i in range(p+1, r):
-		if A[i-1] > 0 :
+		if firmeza[i-1] > 0 :
 			firmeza[i] = firmeza[i-1] + A[i]
 		else:
 			firmeza[i] = A[i]
@@ -21,32 +21,30 @@ def solidezV3(A, p, r) :
 
 # função utilizada na versão 2 do solidez
 def solidez_meio(A, p, q, r):
-	B = [None]*r
-	B = A
-	aux = s = B[q]
 
-	for i in range(q-1, p, -1):
-		s += B[i]
+	aux = s = A[q]
+
+	for i in range(q-1, p-1, -1):
+		s += A[i]
 
 		if s > aux :
 			aux = s
 
-	aux2 = s = B[q+1]
+	aux2 = s = A[q+1]
 
-	for j in range(q+2, r):
-		s += B[j]
+	for j in range(q+2, r+1):
+		s += A[j]
 
 		if s > aux2:
-			aux2 = aux
+			aux2 = s
 
 	return aux + aux2
 
 
 # implementa a solução para o problema da solidez usando a técnica da divisão e conquista
 def solidezV2(A, p, r):
-  
-	valores = [0]*r
-	
+	valores = []
+
 	if p == r:
 		return A[p]
 
@@ -56,42 +54,54 @@ def solidezV2(A, p, r):
 		y = solidezV2(A, q+1, r)
 		z = solidez_meio(A, p, q, r)
 
+
 		valores.append(x)
 		valores.append(y)
 		valores.append(z)
 
+		# print(valores)
 		return max(valores)
 
 def solidezV1(A, p, r):
-  soma = [0]*r
+	soma = []
 
-  # se o intervalo é unitário
-  if p == r:
-    return A[p]
+	# se o intervalo é unitário
+	if p == r:
+		return A[p]
 
-  i, j = p, 0
-  
-  while (j < r):
-    
-    while (i < r) and (A[i] >= 0):
-      soma[j] += A[i]
-      i += 1
-	  
-	# se somou até o número máximo de elementos
-    if i == r:
-      # sai do primeiro laço
-      j = r
-    # se ainda restam elementos para análise 
-    else:
-      j += 1
-      # incrementa o i para a continuação do cálculo
-      i += 1
-  
-  return max(soma)
+	i, j = p, 0
+
+	while (j < r):
+		aux = 0
+		i = r - j -1
+		while (i >= 0):
+			aux += A[i]
+			i -= 1
+
+		soma.append(aux)
+
+		j += 1
+		print(soma)
+
+	j = 0
+
+	while (j < r):
+		aux = 0
+		i = j
+		while (i < r):
+	  		aux += A[i]
+	  		i += 1
+
+		soma.append(aux)
+
+		j += 1
+		print(soma)
+
+	return max(soma)
 
 def main ():
-  a = [1,1,1,0,-1,1]
-  print("soma máxima:", solidezV2(a, 0, len(a)))
+  a = [0, 1, 1, -1,-1]
+  print("soma máxima:", solidezV1(a, 0, len(a)))
   
   
 main()
